e45cc6c39fe116089fad95e0ee70d613ce1d93cd
alex <alex_kobilianskiy@bigmir.net>
2019-11-20 10:26:43 +0200

    section houses complete

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
+    $('.house-slider').slick({
+        infinite: true,
+        fade: true,
+        cssEase: 'linear',
+        arrows: true,
+        prevArrow: '<button type="button" class="house-prev"></button>',
+        nextArrow: '<button type="button" class="house-next"></button>',
+    });
+
+    $('.house-nav').click(function() {
+        $('.house-slider').slick('slickGoTo',$(this).index());
+    });
+
     //E-mail Ajax Send
     $("form").submit(function() { //Change
         var th = $(this);
