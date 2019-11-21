eda57d28ff527223a5c6759006c078adf5b40c53
alex <alex_kobilianskiy@bigmir.net>
2019-11-21 03:11:23 +0200

    section additional complete

--- app/js/common.js
+++ app/js/common.js
@@ -9,6 +9,50 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="house-next"></button>',
     });
 
+    $('.add-slider').slick({
+        infinite: true,
+        arrows: true,
+        prevArrow: '<button type="button" class="house-prev"></button>',
+        nextArrow: '<button type="button" class="house-next"></button>',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 390,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    function heightses() {
+        if ($(window).width()<480) {
+
+        }
+
+        $('.add-item-content').height('auto').equalHeights();
+    }
+
+    $(window).resize(function() {
+        heightses();
+    });
+
+    heightses();
+
     $('.house-nav').click(function() {
         $('.house-slider').slick('slickGoTo',$(this).index());
     });
